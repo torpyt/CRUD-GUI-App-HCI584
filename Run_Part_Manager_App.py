320ff9d8d2d984a55dfe91f42861f44e70469548
torpyt <106179967+torpyt@users.noreply.github.com>
2022-07-28 13:17:09 -0500

    Update Run_Part_Manager_App.py
    Added more #comments to the code

--- Run_Part_Manager_App.py
+++ Run_Part_Manager_App.py
@@ -4,13 +4,14 @@ from db import Database
 
 db = Database('store.db')
 
-
+# Function that populates a list
 def populate_list():
     parts_list.delete(0, END)
     for row in db.fetch():
         parts_list.insert(END, row)
 
-
+# Function that allows items to be added to list
+# Function that shows error messagebox if there is no data in input fields when user presses add item button
 def add_item():
     if part_text.get() == '' or customer_text.get() == '' or retailer_text.get() == '' or price_text.get() == '':
         messagebox.showerror('Required Fields', 'Please include all fields')
@@ -23,7 +24,7 @@ def add_item():
     clear_text()
     populate_list()
 
-
+# Function that selects item from list
 def select_item(event):
     try:
         global selected_item
@@ -41,19 +42,19 @@ def select_item(event):
     except IndexError:
         pass
 
-
+# Function that removes item from list
 def remove_item():
     db.remove(selected_item[0])
     clear_text()
     populate_list()
 
-
+# Function that updates item from list
 def update_item():
     db.update(selected_item[0], part_text.get(), customer_text.get(),
               retailer_text.get(), price_text.get())
     populate_list()
 
-
+#Function that clears input boxes
 def clear_text():
     part_entry.delete(0, END)
     customer_entry.delete(0, END)
@@ -61,59 +62,72 @@ def clear_text():
     price_entry.delete(0, END)
 
 
-# Create window object
+# Creates GUI window
 app = Tk()
 
-# Part
+# Part input box
 part_text = StringVar()
 part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
 part_label.grid(row=0, column=0, sticky=W)
 part_entry = Entry(app, textvariable=part_text)
 part_entry.grid(row=0, column=1)
-# Customer
+
+# Customer input box
 customer_text = StringVar()
 customer_label = Label(app, text='Customer', font=('bold', 14))
 customer_label.grid(row=0, column=2, sticky=W)
 customer_entry = Entry(app, textvariable=customer_text)
 customer_entry.grid(row=0, column=3)
-# Retailer
+
+# Retailer input box
 retailer_text = StringVar()
 retailer_label = Label(app, text='Retailer', font=('bold', 14))
 retailer_label.grid(row=1, column=0, sticky=W)
 retailer_entry = Entry(app, textvariable=retailer_text)
 retailer_entry.grid(row=1, column=1)
-# Price
+
+# Price input box
 price_text = StringVar()
 price_label = Label(app, text='Price', font=('bold', 14))
 price_label.grid(row=1, column=2, sticky=W)
 price_entry = Entry(app, textvariable=price_text)
 price_entry.grid(row=1, column=3)
+
 # Parts List (Listbox)
 parts_list = Listbox(app, height=8, width=50, border=0)
 parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+
 # Create scrollbar
 scrollbar = Scrollbar(app)
 scrollbar.grid(row=3, column=3)
+
 # Set scroll to listbox
 parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
+
 # Bind select
 parts_list.bind('<<ListboxSelect>>', select_item)
 
-# Buttons
+# Add button
 add_btn = Button(app, text='Add Part', width=12, command=add_item)
 add_btn.grid(row=2, column=0, pady=20)
 
+#Remove button
 remove_btn = Button(app, text='Remove Part', width=12, command=remove_item)
 remove_btn.grid(row=2, column=1)
 
+#Update button
 update_btn = Button(app, text='Update Part', width=12, command=update_item)
 update_btn.grid(row=2, column=2)
 
+#Clear input button
 clear_btn = Button(app, text='Clear Input', width=12, command=clear_text)
 clear_btn.grid(row=2, column=3)
 
+#Title of app, seen in the app window
 app.title('Part Manager')
+
+#Size of the app window
 app.geometry('700x350')
 
 # Populate data
